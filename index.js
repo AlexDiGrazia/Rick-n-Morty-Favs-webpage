d51a5c43f10d77f4d20ec0580a5331e95b787719
Alex Digrazia <alexndigrazia@hotmail.com>
2023-01-31 03:28:24 -0800

    change Javascript from inline CSS styles to class based add/remove CSS styles, and refactor CSS/ remove un-used CSS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const rick = fetch(
-  "https://rickandmortyapi.com/api/character/17, 4, 60, 89, 118, 126, 141, 152, 656, 5, 191, 202, 2, 252, 254, 265, 274, 1, 3, 353, 372, 440, 382, 721, 383, 388"
+  "https://rickandmortyapi.com/api/character/17, 4, 60, 89, 118, 126, 141, 152, 5, 191, 202, 2, 252, 254, 265, 274, 1, 3, 353, 372, 440, 382, 721, 383, 388"
 );
 
 let array = [];
@@ -10,7 +10,7 @@ rick
   .then((data) => data.json())
   .then((data) => data.forEach((element) => makeArray(element)))
   .then((data) => array.forEach((person) => makeCard(person, ".main", '<span>♡</span>')))
-  .then((data) => loop());
+  .then((data) => displayDataFunction());
 
 function makeArray(character) {
   array.push(character);
@@ -24,10 +24,10 @@ function makeCard(element, collection, button) {
     <div class= "image-wrapper">
       <img src=${element.image} alt=${element.name}>
       <div class="overlay">
-        <h6 class="someText">Species: ${element.species}</h6>
-        <h6 class="someText">Origin: ${element.origin.name}</h6>
-        <h6 class="someText">Status: ${element.status}</h6>
-        <button onclick="favoritesClick()" class="before-like">${button}</button>
+        <h6 class="card-stats">Species: ${element.species}</h6>
+        <h6 class="card-stats">Origin: ${element.origin.name}</h6>
+        <h6 class="card-stats">Status: ${element.status}</h6>
+        <button onclick="likeButton()" class="like-button">${button}</button>
       </div>
     </div>
     <h3>${element.name}</h3>
@@ -35,53 +35,73 @@ function makeCard(element, collection, button) {
   document.querySelector(collection).appendChild(card);
 }
 
-function favoritesClick() {
-  const whereIsIt =
+function likeButton() {
+  const parentContainer =
     event.target.parentElement.parentElement.parentElement.parentElement.parentElement;
   const cardClicked = event.target.parentElement.parentElement.parentElement.parentElement;
   const id = cardClicked.id;
-  if (whereIsIt.classList.value.includes("main")) {
-    switchCollections(
+  if (parentContainer.classList.value.includes("main")) {
+    switchCollectionsArray(
       id,
       array,
       favoritesArray
     ); 
     cardClicked.remove("div");
     document.querySelector(".favorites").appendChild(cardClicked);
-    cardClicked.querySelector('.image-wrapper').querySelector('.overlay').querySelector('.before-like').remove('button');
+    cardClicked.querySelector('.image-wrapper').querySelector('.overlay').querySelector('.like-button').remove('button');
     const xOutButton = document.createElement('button');
-    xOutButton.setAttribute("onclick", "favoritesClick()");
-    xOutButton.className = "before-like"
+    xOutButton.setAttribute("onclick", "likeButton()");
+    xOutButton.className = "like-button"
     xOutButton.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
     cardClicked.querySelector('.image-wrapper').querySelector('.overlay').append(xOutButton);
-  } else if (whereIsIt.classList.value.includes("favorites")) {
-    switchCollections(
+  } else if (parentContainer.classList.value.includes("favorites")) {
+    switchCollectionsArray(
       id,
       favoritesArray,
       array
     );
     cardClicked.remove("div");
     document.querySelector(".main").appendChild(cardClicked);
-    cardClicked.querySelector('.image-wrapper').querySelector('.overlay').querySelector('.before-like').remove('button');
+    cardClicked.querySelector('.image-wrapper').querySelector('.overlay').querySelector('.like-button').remove('button');
     const heartButton = document.createElement('button');
-    heartButton.setAttribute("onclick", "favoritesClick()");
-    heartButton.className = "before-like"
+    heartButton.setAttribute("onclick", "likeButton()");
+    heartButton.className = "like-button"
     heartButton.innerHTML = `<span>♡</span>`;
     cardClicked.querySelector('.image-wrapper').querySelector('.overlay').append(heartButton);
   }
-  loop();
+  displayDataFunction();
 }
 
-//switches arrays
-
-function switchCollections(cardId, whichArray, whereTo) {
+function switchCollectionsArray(cardId, whichArray, whereTo) {
   const characterToMove = whichArray.find((character) => character.name === cardId); 
   const index = whichArray.findIndex((object) => object.name === characterToMove.name); 
   whichArray.splice(index, 1);
   whereTo.push(characterToMove);
 }
 
-//Alphabetical sort
+function displayDataFunction() {
+  document.querySelector('.main-data').innerHTML = `<h4><strong>Main Data</strong></h4>`;
+  document.querySelector('.favorite-data').innerHTML = `<h4><strong>Favorites Data</strong></h4>`;
+  const dataValueArrays = [
+    ['status', 'Alive', 'Status Alive' ], 
+    ['status', 'Dead', 'Status Dead'], 
+    ['species', 'Human', 'Species Human'], 
+    ['species', 'Alien', 'Species Alien']
+  ];
+  
+  for(let value of dataValueArrays) {
+    appendCharacterDataToDropDownMenu(...value, array, '.main-data');
+  }
+  for(let value of dataValueArrays) {
+    appendCharacterDataToDropDownMenu(...value, favoritesArray, '.favorite-data')
+  }
+}
+  
+function appendCharacterDataToDropDownMenu(key, value, message, whichArray, appendWhere) {
+  let thisNumber = whichArray.filter((person) => person[key] === value);
+  document.querySelector(appendWhere).innerHTML += `${message}: ${thisNumber.length}<br><br>`;
+}
+
 function alphabetize() {
   array.sort((a, b) => (a.name > b.name ? 1 : -1));
   favoritesArray.sort((a, b) => (a.name > b.name ? 1 : -1));
@@ -91,7 +111,7 @@ function alphabetize() {
 }
 
 function zThroughA() {
-  array.sort((a, b) => (a.name < b.ename ? 1 : -1));
+  array.sort((a, b) => (a.name < b.name ? 1 : -1));
   favoritesArray.sort((a, b) => (a.name < b.name ? 1 : -1));
   deleteAll();
   array.forEach((person) => makeCard(person, ".main", '<span>♡</span>'));
@@ -99,50 +119,22 @@ function zThroughA() {
 }
 
 function deleteAll() {
-  const mainCollection = document.querySelector(".main");
-  const favCollection = document.querySelector(".favorites");
-  mainCollection.innerHTML = "";
-  favCollection.innerHTML = "";
+  document.querySelector(".main").innerHTML = "";
+  document.querySelector(".favorites").innerHTML = `
+  <a id="scrollUpFavorites" class="border button-styles" href="#pageTopFavorites">
+    <i class="fa-solid fa-arrow-up-long"></i>
+  </a> `;
 }
-
-
-function statusValues(key, value, message, whichArray, appendWhere) {
-    let thisNumber = whichArray.filter((person) => person[key] === value);
-    document.querySelector(appendWhere).innerHTML += `${message}: ${thisNumber.length}<br><br>`;
-}
-
-function loop() {
-  document.querySelector('.main-data').innerHTML = `<h4><strong>Main Data</strong></h4>`;
-  document.querySelector('.favorite-data').innerHTML = `<h4><strong>Favorites Data</strong></h4>`;
-  const dataValueArrays = [
-    ['status', 'Alive', 'Status Alive' ], 
-    ['status', 'Dead', 'Status Dead'], 
-    ['species', 'Human', 'Species Human'], 
-    ['species', 'Alien', 'Species Alien']
-  ];
-
-  for(let value of dataValueArrays) {
-    statusValues(...value, array, '.main-data');
-  }
-  for(let value of dataValueArrays) {
-    statusValues(...value, favoritesArray, '.favorite-data')
-  }
-}
-
-//Change page
 
 function goToFavoritesCollection() {
-  document.querySelector('.favorites-collection').style.display = 'block';
-  document.getElementById('scrollUp').style.display = 'none';
+  document.querySelector('.favorites-collection').classList.remove('display-none');
+  document.getElementById('scrollUp').classList.add('display-none');
 }
 
 function returnToMainCollection() {
-  document.querySelector('.favorites-collection').style.display = 'none';
-  document.getElementById('scrollUp').style.display = 'block';
+  document.querySelector('.favorites-collection').classList.add('display-none');
+  document.getElementById('scrollUp').classList.remove('display-none');
 }
-
-
-//Click Data open/close
 
 function openDataDropDown(id, button) {
   document.getElementById(id).classList.add('translateY-down');
@@ -156,17 +148,15 @@ function closeDataDropDown(id, button) {
   document.getElementById(button).setAttribute("onclick", `openDataDropDown('${id}', '${button}')`);
 }
 
-// Click slider open/close
-
 function sliderTabOpen() {
-  if(document.querySelector('.data-dropdown-slider-main-collection').classList.value.includes('translateX-left')) {
-    document.querySelector('.data-dropdown-slider-main-collection').classList.remove('translateX-left');
+  if(document.querySelector('.data-dropdown-slider-main-collection').classList.value.includes('translateX-left-and-up')) {
+    document.querySelector('.data-dropdown-slider-main-collection').classList.remove('translateX-left-and-up');
   }
-  if(document.querySelector('.data-dropdown-slider-fav-collection').classList.value.includes('translateX-left')) {
-    document.querySelector('.data-dropdown-slider-fav-collection').classList.remove('translateX-left');
+  if(document.querySelector('.data-dropdown-slider-fav-collection').classList.value.includes('translateX-left-and-up')) {
+    document.querySelector('.data-dropdown-slider-fav-collection').classList.remove('translateX-left-and-up');
   }
  Array.from(document.querySelectorAll('.tab-slide-function'))
- .forEach((button) => button.style.transform = "none");
+ .forEach((button) => button.classList.remove("translateX-left"));
  Array.from(document.querySelectorAll('.slider-tab-open'))
   .forEach((button) => button.innerHTML =`<i class="fa-solid fa-chevron-left"></i>`);
   Array.from(document.querySelectorAll('.slider-tab-open'))
@@ -175,14 +165,14 @@ function sliderTabOpen() {
 
 function sliderTabClose() {
   Array.from(document.querySelectorAll('.tab-slide-function'))
-  .forEach((button) => button.style.transform = "translateX(-232px)");
+  .forEach((button) => button.classList.add("translateX-left"));
   if(document.getElementById('main-data-menu').classList.value.includes('translateY-down')) {
     closeDataDropDown('main-data-menu', 'main-data-dropdown')
-    document.getElementById('main-data-menu').classList.add('translateX-left');
+    document.getElementById('main-data-menu').classList.add('translateX-left-and-up');
   }
   if(document.getElementById('fav-data-menu').classList.value.includes('translateY-down')) {
     closeDataDropDown('fav-data-menu', 'fav-data-dropdown')
-    document.getElementById('fav-data-menu').classList.add('translateX-left');
+    document.getElementById('fav-data-menu').classList.add('translateX-left-and-up');
   }
   Array.from(document.querySelectorAll('.slider-tab-open'))
   .forEach((button) => button.innerHTML =`<i class="fa-solid fa-chevron-right"></i>`);
@@ -190,14 +180,12 @@ function sliderTabClose() {
   .forEach((button) => button.setAttribute("onclick", "sliderTabOpen()"));
  }
 
- //Scroll slider open/close
-
  document.querySelector('.favorites-collection').addEventListener("scroll", (evt) => {
   if(evt.target.scrollTop > 220) {
       sliderTabClose();
    } else {
     Array.from(document.querySelectorAll('.tab-slide-function'))
-  .forEach((button) => button.style.transform = "none");
+  .forEach((button) => button.classList.remove('translateX-left'));
   document.querySelector('.slider-tab-open').innerHTML = `<i class="fa-solid fa-chevron-left"></i>`;
   sliderTabOpen();
    }
@@ -209,7 +197,7 @@ function sliderTabClose() {
     sliderTabClose();
    } else {
     Array.from(document.querySelectorAll('.tab-slide-function'))
-  .forEach((button) => button.style.transform = "none");
+  .forEach((button) => button.classList.remove('translateX-left'));
   document.querySelector('.slider-tab-open').innerHTML = `<i class="fa-solid fa-chevron-left"></i>`;
   sliderTabOpen();
    }
@@ -217,8 +205,4 @@ function sliderTabClose() {
  }
  onScrollHide();
 
-document.getElementById('scrollUp').addEventListener("scroll", (evt) => {
-  if(evt.target.scrollTop > 4) {
-    console.log('yolo')
-  }
-})
+
